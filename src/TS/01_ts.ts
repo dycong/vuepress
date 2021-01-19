9fae8f29bc7dacf35bb322b982aedb78c7e185d5
dycong <dycong0715@163.com>
2021-01-19 17:43:55 +0800

    添加ts vue.config.js

---
+++ src/TS/01_ts.ts
@@ -0,0 +1,19 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: any, public middleInitial: any, public lastName: any) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+function greeter(person : Person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("Jane", "M.", "User");
+
+document.body.innerHTML = greeter(user);
